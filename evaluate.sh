c1b9579447a1081322965d848c3ffa862036e450
Parth Chopra <parthc@umich.edu>
2020-09-04 17:26:14 -0400

    float32multiarray implementation - shows garbage output

---
+++ evaluate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./demo.py -d 0 config/wireframe.yaml ./checkpoint/190418-201834-f8934c6-lr4d10-312k.pth.tar /home/ros/ts_june_ws/src/ros-semantic-segmentation/semantic_segmentation/nodes/static_img.jpg 
